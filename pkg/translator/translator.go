51ce27bf314bfcf37a8b6a5b68425ed41c384cb6
joe <josepht404@gmail.com>
2019-02-23 10:22:37 -0800

    Update the translator

--- pkg/translator/translator.go
+++ pkg/translator/translator.go
@@ -1,11 +1,37 @@
 package translator
 
 import (
+	"lang/pkg/script"
 	"lang/pkg/parser"
 	"lang/pkg/"
 )
 
-func defineIF(prgm) {
+func Translate(node *parser.Expr)[]code {
+
+}
+
+
+func translateIF(code []byte, node *parser.FnCall) []byte {
+	// Assert the validity of IF.
+	if len(node.Args) != 3 {
+		panic(fmt.Sprintf(
+			"If does not contain enough arguments should be in the form\n %s",
+			"(if (...conditional) (...) (...))\n"
+		))
+	}
+
+	// Add in the IF statement.
+	code = append(
+		code,
+		Translate(node.Args[0])
+		script.OP_IF,
+		Translate(node.Args[1])
+		script.OP_ELSE,
+		Translate(node.Args[2])
+		script.OP_ENDIF,
+	)
+
+	return code
 }
 
 
--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -1,7 +1,6 @@
 package validator
 
 import (
-<<<<<<< HEAD
 	"fmt"
 
 	"lang/pkg/parser"
@@ -64,26 +63,3 @@ func validateFn (def parser.FunctionDecl) {
 	}
 }
 
-=======
-	"lang/pkg/parser"
-)
-
-
-func Validate(program parser.Program) {
-
-	// Program must have a main function
-	if program.Main == nil {
-		panic("Please insert a main function into your program\n"
-		+ "i.e. (main () true)")
-	}
-
-	for def,_ := range program.Definitions {
-		validateFn(def.Name, def.Args, def.Expr)
-	}
-}
-
-func validateFn(name string, args parser.Expr, body parser.Expr) {
-
-	// There should only be T
-}
->>>>>>> joe/interpreter
