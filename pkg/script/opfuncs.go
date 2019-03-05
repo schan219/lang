68dfb41ca707795ce4444c8350cb09035df7cdb8
joe <josepht404@gmail.com>
2019-03-04 22:23:32 -0800

    Add factory for pushing in data

--- pkg/script/opfuncs.go
+++ pkg/script/opfuncs.go
@@ -5,7 +5,39 @@ import (
 )
 
 var OP_FUNCS = map[int]interface{} {
+	OP_0:         pushData(-1,1),
+
+	OP_PUSHDATA1: pushData(1,-1),
+	OP_PUSHDATA2: pushData(2,-1),
+	OP_PUSHDATA4: pushData(4,-1),
 }
+
+func init () {
+	// we go from OP_DATA_1, to OP_DATA_75
+	for opcode := OP_DATA_1; opcode <= OP_DATA_75; opcode++ {
+		OP_FUNCS[opcode] = pushData(opcode, -1);
+	}
+
+	// We go from OP_1, to OP_16 inclusize
+	for value := 1; value <= 16; value++ {
+		// Since we start at 1, OP_1+1-1 == OP_1.
+		OP_FUNCS[OP_1 + value - 1] = pushData(-1, value); 
+	}
+}
+
+func pushData (width int, value int) interface{} {
+	// We just return the next n 
+	if (value == -1) {
+		return func () {
+			
+		}
+	} else {
+		return func () {
+
+		}
+	}
+}
+
 
 func pushDataCmd (intp *Interpreter, script []byte, cmd byte) error {
 	if int(cmd) > len(script) {
