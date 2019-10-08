ec27d28f8466f699d7f6cd717dfea1d8c72409b0
UBC Student <joey6978@valdes.students.cs.ubc.ca>
2019-10-07 18:51:32 -0700

    Continuing with interpreter functionality.. slow

--- pkg/script/interpreter.go
+++ pkg/script/interpreter.go
@@ -5,6 +5,14 @@ import (
 )
 
 // Executes a provided string ignoring CHECKSIG operations
-func Execute(script []byte) {
+func Execute(script []byte, starting [][]byte) {
+	// TODO: Check for CHECKSIGS and complain.
+	msgTx := NewMsgTx(1)
+	msgTx.AddTxOut(&TxOut{
+		Value: 0,
+		PkScript: script
+	})
+	msgTx.LockTime = 0
 
+	return txscript.NewEngine(script, msgTx, 0, nil, nil, 1).Execute()
 }
